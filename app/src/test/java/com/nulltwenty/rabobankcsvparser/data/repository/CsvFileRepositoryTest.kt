38fd03be952b606a4f2af00ed2c5d37759ce2d5b
Manuel Lorenzo <manulorenzop@gmail.com>
2022-11-05 09:30:22 +0100

    add a repository test for a successful response

--- app/src/test/java/com/nulltwenty/rabobankcsvparser/data/repository/CsvFileRepositoryTest.kt
+++ app/src/test/java/com/nulltwenty/rabobankcsvparser/data/repository/CsvFileRepositoryTest.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
 import okhttp3.ResponseBody.Companion.toResponseBody
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Assert.fail
 import org.junit.Test
@@ -35,6 +36,33 @@ class CsvFileRepositoryTest {
                         val bytes = result.data.readBytes()
                         val text = String(bytes, StandardCharsets.UTF_8)
                         assertTrue(text.isEmpty())
+                    }
+                }
+            }
+        }
+
+    @Test
+    fun `given a CsvFileService when it returns an successful response, the repository should return the string in a ResultOf#Success`() =
+        runTest {
+            val testString = """"First name","Sur name","Issue count","Date of birth","avatar"
+"Theo","Jansen",5,"1978-01-02T00:00:00","https://api.multiavatar.com/2cdf5db9b4dee297b7.png"
+"Fiona","de Vries",7,"1950-11-12T00:00:00","https://api.multiavatar.com/b9339cb9e7a833cd5e.png"
+"Petra","Boersma",1,"2001-04-20T00:00:00","https://api.multiavatar.com/2672c49d6099f87274.png""""
+            csvFileServiceMock = mock {
+                onBlocking { fetchCsvFile() } doSuspendableAnswer {
+                    Response.success(
+                        testString.toResponseBody()
+                    )
+                }
+            }
+            sut = CsvFileRepositoryImpl(testDispatcher, csvFileServiceMock)
+            sut.fetchCsvFile().collect { result ->
+                when (result) {
+                    is ResultOf.Error -> fail("This case should never be an error")
+                    is ResultOf.Success -> {
+                        val bytes = result.data.readBytes()
+                        val text = String(bytes, StandardCharsets.UTF_8)
+                        assertEquals(text, testString)
                     }
                 }
             }
